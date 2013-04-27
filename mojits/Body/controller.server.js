0b31ab5df44a5e8e7fb9c1ec6490748b41941653
rubelux <rubelux@gmail.com>
2013-04-27 12:05:44 +0100

    adding rooting and bootstrap and mojits structure
    Adding all the structure, body page is a htmlFrame mojit with 3 sub
    mojits, nav, content, footer...
    
    + boostrap

---
+++ mojits/Body/controller.server.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2012 Yahoo! Inc. All rights reserved.
+ */
+/*jslint anon:true, sloppy:true, nomen:true*/
+YUI.add('Body', function(Y, NAME) {
+
+/**
+ * The Body module.
+ *
+ * @module Body
+ */
+
+    /**
+     * Constructor for the Controller class.
+     *
+     * @class Controller
+     * @constructor
+     */
+    Y.namespace('mojito.controllers')[NAME] = {
+
+        /**
+         * Method corresponding to the 'index' action.
+         *
+         * @param ac {Object} The ActionContext that provides access
+         *        to the Mojito API.
+         */
+        index: function(ac) {
+            
+            ac.composite.done();
+        }
+
+    };
+
+}, '0.0.1', {requires: ['mojito', 'mojito-composite-addon']});
